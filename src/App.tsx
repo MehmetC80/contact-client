eb62821df3e2c6ef5a48bb361149817e97d2dbdc
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2023-12-10 17:50:09 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen bg-slate-950 text-white flex flex-col pt-12">
+      <h1 className=" text-4xl text-center  font-bold">Kontakt-App</h1>
+    </div>
+  );
+}
+
+export default App;
